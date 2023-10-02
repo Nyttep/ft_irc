c080505239150b61194a1ba7bfb6b6a875925d4c
Maxime PORTRAIT <mportrai@bess-f1r4s2.clusters.42paris.fr>
2023-10-02 15:33:39 +0200

    check_command

--- srcs/command/check_command.cpp
+++ srcs/command/check_command.cpp
@@ -15,6 +15,39 @@ std::vector<std::string>	collect_arguments(std::string string)
 	return (vector);
 }
 
+bool	correct_nick_chan(std::string name)
+{
+	if (chantypes(name[0]) == true)
+	{
+		for (size_t i = 0; i != name.length(); ++i)
+		{
+			if (name[i] == ' ' || name[i] == '^G' || name[i] == ',')
+			{
+				std::cerr << "redirection  432" << std::endl;
+				return (false);
+			}
+		}
+	}
+	else
+	{
+		if (name[0] == '$' || name[0] == '#' || name[0] == '&' || name[0] == '%' || (isdigit(name[0])))
+		{
+			std::cerr << "Redirection 432" << std::endl;
+			return (false);
+		}
+		for (size_t i = 0; i != name.length(); ++i)
+		{
+			if (name[i] == ' ' || name[i] == ','|| name[i] == '.' || name[i] == '*' || \
+				name[i] == '?' || name[i] == '!' || name[i] == '@')
+			{
+				std::cerr << "Redirection 432" << std::endl;
+				return (false);
+			}
+		}
+	}
+	return (true);
+}
+
 std::string	store_message(Command command)
 {
 	std::string message;
@@ -28,6 +61,28 @@ std::string	store_message(Command command)
 		}
 	}
 	return (message);
+}
+
+std::string empty_param(std::vector<std::string> params, size_t i)
+{
+	if (params.size() < i)
+		return ("");
+	return (params[i]);
+}
+
+bool	analyse_param(std::string param)
+{
+	if (param.size() != 2)
+	{
+		sendAll(RPL_INVALIDMODEPARAN("@"+command.getSource(), command.getParams()[0], command.getParams()[1], empty_param(command.getParams(), 2), " : One mode char ((+/-)(i/t/k/l/o))\r\n"), command.getSource());
+		return (false);
+	}
+	if (param[0] != '+' && param[0] != '-')
+	{
+		sendAll(RPL_INVALIDMODEPARAN("@"+command.getSource(), command.getParams()[0], command.getParams()[1], empty_param(command.getParams(), 2), " : Incorrect mode char ((+/-)(i/t/k/l/o))\r\n"), command.getSource());
+		return (false);
+	}
+	return (true);
 }
 
 std::string	DateCreation()
@@ -53,12 +108,12 @@ void	handshake(Command command, Server server)
 	sendAll(RPL_ISUPPORT(SERVERNAME, "CHANTYPES=" + CHANTYPES), command.getSource());
 	sendAll(RPL_ISUPPORT(SERVERNAME, "CHANLIMIT=" + CHANLIMIT), command.getSource());
 	sendAll(RPL_ISUPPORT(SERVERNAME, "CHANMODES=" + CHANMODES), command.getSource());
-	sendAll(RPL_ISUPPORT(SERVERNAME, "MODE=" + std::atoi(MODES)), command.getSource());
+	sendAll(RPL_ISUPPORT(SERVERNAME, "MODE=" + std::itoa(MODES)), command.getSource());
 	sendAll(RPL_ISUPPORT(SERVERNAME, "PREFIX=" + PREFIX), command.getSource());
 	sendAll(RPL_ISUPPORT(SERVERNAME, "TARGMAX=" + TARGMAX), command.getSource());
-	sendAll(RPL_ISUPPORT(SERVERNAME, "NICKLEN=" + std::atoi(NICKLEN)), command.getSource());
-	sendAll(RPL_ISUPPORT(SERVERNAME, "USERLEN=" + std::atoi(USERLEN)), command.getSource());
-	sendAll(RPL_ISUPPORT(SERVERNAME, "CHANNELLEN=" + std::atoi(CHANNELLEN)), command.getSource());
-	sendAll(RPL_ISUPPORT(SERVERNAME, "KICKLEN=" + std::atoi(KICKLEN)), command.getSource());
-	sendAll(RPL_ISUPPORT(SERVERNAME, "TOPICLEN=" + std::atoi(TOPICLEN)), command.getSource());
+	sendAll(RPL_ISUPPORT(SERVERNAME, "NICKLEN=" + std::itoa(NICKLEN)), command.getSource());
+	sendAll(RPL_ISUPPORT(SERVERNAME, "USERLEN=" + std::itoa(USERLEN)), command.getSource());
+	sendAll(RPL_ISUPPORT(SERVERNAME, "CHANNELLEN=" + std::itoa(CHANNELLEN)), command.getSource());
+	sendAll(RPL_ISUPPORT(SERVERNAME, "KICKLEN=" + std::itoa(KICKLEN)), command.getSource());
+	sendAll(RPL_ISUPPORT(SERVERNAME, "TOPICLEN=" + std::itoa(TOPICLEN)), command.getSource());
 }
