d7ac4585c6276fc119d8cd6e9adab2bd2b4070bb
Maxime PORTRAIT <mportrai@bess-f1r4s1.clusters.42paris.fr>
2023-10-03 11:38:38 +0200

    supp d'une condition de ping

--- srcs/command/ping.cpp
+++ srcs/command/ping.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ping.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pdubois <pdubois@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mportrai <mportrai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 16:41:57 by mportrai          #+#    #+#             */
-/*   Updated: 2023/10/02 18:45:59 by pdubois          ###   ########.fr       */
+/*   Updated: 2023/10/03 11:38:18 by mportrai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	execute_PING(Command &command)
 {
-	if (command.getParams().empty() || command.getParams()[0].empty() || command.getParams().size() != 1)
+	if (command.getParams().empty() || command.getParams()[0].empty())
 	{
 		sendAll(ERR_NEEDMOREPARAMS(command.getSource()->getNName(), command.getVerb()), *command.getSource());
 		std::cerr << "Redirection 461" << std::endl;
